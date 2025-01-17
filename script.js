1a37e2eaf9caa0bb4dff57daa6dba2092922d968
nischithagowda <nischitha@togglr.com>
2025-01-17 21:57:20 +0530

    enhance navbar functionality and improve scroll behavior

--- script.js
+++ script.js
@@ -1,57 +1,66 @@
 $(document).ready(function() {
+    // Sticky navbar and scroll-up button
     $(window).scroll(function() {
-        if (this.scrollY > 20) {
+        if ($(this).scrollTop() > 50) {
             $('.navbar').addClass("sticky");
         } else {
             $('.navbar').removeClass("sticky");
         }
 
-        if (this.scrollY > 500) {
+        if ($(this).scrollTop() > 500) {
             $('.scroll-up-btn').addClass("show");
         } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
+    // Scroll to top on click
     $('.scroll-up-btn').click(function() {
-        $('html').animate({scrollTop: 0});
-        $('html').css("scrollBehavior", "auto");
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
     });
 
+    // Smooth scroll for navbar links
     $('.navbar .menu li a').click(function() {
-        $('html').css("scrollBehavior", "smooth");
+        $('.navbar .menu').removeClass("active");
+        $('.menu-btn i').removeClass("active");
     });
 
-    $('.menu-btn').click(function() {
+    // Navbar toggle
+    $('.menu-btn').on('click touchstart', function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-    
+    // Typing text animation
+    var stringsArray = window.innerWidth < 768 
+        ? ["Python Developer", "ML Developer"] 
+        : ["Python Developer", "ML Developer", "Freelance Trainer"];
+
     var typed = new Typed(".typing", {
-        strings: [ "Python Developer", "ML Developer", "Freelance Trainer"],
+        strings: stringsArray,
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
-    // var typed = new Typed(".typing-2", {
-    //     strings: [ "Web Developer", "UI/UX Designer", "Software Developer", "Content Writer","Freelancer"],
-    //     typeSpeed: 100,
-    //     backSpeed: 60,
-    //     loop: true
-    // });
-
+    // OwlCarousel initialization
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
         autoplay: true,
-        autoplayTimeOut: 2000,
+        autoplayTimeout: 2000,
         autoplayHoverPause: true,
         responsive: {
             0: {
                 items: 1,
                 nav: false
+            },
+            400: {
+                items: 1,
+                nav: true
             },
             600: {
                 items: 2,
@@ -65,19 +74,14 @@ $(document).ready(function() {
     });
 });
 
-
-
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-function opentab(tabname) {
-  for (tablink of tablinks) {
-    tablink.classList.remove("active-link");
-  }
-  for (tabcontent of tabcontents) {
-    tabcontent.classList.remove("active-tab");
-  }
-  
-  event.currentTarget.classList.add("active-link");
-  document.getElementById(tabname).classList.add("active-tab");
+// Tabs functionality
+function opentab(tabname, event) {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
 }
-
